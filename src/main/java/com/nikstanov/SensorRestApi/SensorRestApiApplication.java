82fe5e8b022e93ec6d9469ca7a2483fa7481418c
Nikstanov <N.stahnov@gmail.com>
2023-09-10 15:43:00 +0300

    test function

--- src/main/java/com/nikstanov/SensorRestApi/SensorRestApiApplication.java
+++ src/main/java/com/nikstanov/SensorRestApi/SensorRestApiApplication.java
@@ -1,17 +1,45 @@
 package com.nikstanov.SensorRestApi;
 
+import com.nikstanov.SensorRestApi.dto.MeasurementDTO;
+import com.nikstanov.SensorRestApi.dto.SensorDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Random;
+
 
 @SpringBootApplication
 public class SensorRestApiApplication {
+	static Random random = new Random();
 
 	public static void main(String[] args) {
-		SpringApplication.run(SensorRestApiApplication.class, args);
+		RestTemplate restTemplate = new RestTemplate();
+		SensorDTO sensorDTO = new SensorDTO("sensor2");
+		String registrationURL
+				= "http://localhost:8080/sensors/registration";
+		restTemplate.postForEntity(registrationURL, sensorDTO, ResponseEntity.class);
+
+		String addURL
+				= "http://localhost:8080/measurements/add";
+		for(int i = 0; i < 100; i++){
+			restTemplate.postForEntity(addURL, new MeasurementDTO(random.nextInt(), true, sensorDTO), ResponseEntity.class);
+		}
+
+		String getURL
+				= "http://localhost:8080/measurements";
+		List<MeasurementDTO> results = restTemplate.getForEntity(getURL, List.class).getBody();
+		for(MeasurementDTO measurementDTO : results){
+			System.out.println(measurementDTO.toString());
+		}
+		//SpringApplication.run(SensorRestApiApplication.class, args);
 	}
 
 	@Bean
--- src/main/java/com/nikstanov/SensorRestApi/dto/MeasurementDTO.java
+++ src/main/java/com/nikstanov/SensorRestApi/dto/MeasurementDTO.java
@@ -4,10 +4,22 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.nikstanov.SensorRestApi.utills.JsonBooleanConverter;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class MeasurementDTO {
+    @Override
+    public String toString() {
+        return "MeasurementDTO{" +
+                "value=" + value +
+                ", raining=" + raining +
+                ", sensor=" + sensor.getName() +
+                '}';
+    }
 
     @Min(value = -100, message = "Incorrect value")
     @Max(value = 100, message = "Incorrect value")
